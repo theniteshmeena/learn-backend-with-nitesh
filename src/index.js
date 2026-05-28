48e50743b745df7e50fd49abfc5bc7287918e006
theniteshmeena <theniteshmeena@gmail.com>
2026-05-28 15:30:35 +0530

    Connected to MongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// reqruire('dotenv').config({path: './env'});
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+const app = express()
+;(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on('error', (error) => {
+      console.error('Error connecting to MongoDB:', error)
+            throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`)
+    })
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;    
+  }
+})();
+*/
