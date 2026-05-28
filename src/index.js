b482df7bb84ed2d8eb5c1eb349e53b8e25a05b99
theniteshmeena <theniteshmeena@gmail.com>
2026-05-28 17:30:27 +0530

    Util Code Discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 // reqruire('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+    console.log('Connected to MongoDB');  
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB!!!:', error);
+    throw error;
+});
 
 
 
---
+++ src/utils/asyncHHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+    }
+}
+export {asyncHandler}
+
+
+// Try Catch block for async functions in express routes
+// const asyncHandler = (fn) => async(req, res, next)=>{
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
