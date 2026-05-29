5dc19470caca05ef3bea728f976cfbc62417204d
theniteshmeena <theniteshmeena@gmail.com>
2026-05-29 07:48:47 +0530

    Multer & Cloudinary Files Added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from "cloudinary";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        //upload image to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" }, (error, result) => {
+            if(error) {
+                console.error("Cloudinary upload error:", error);
+                throw error;
+            }
+        });
+        console.log("File uploaded to Cloudinary successfully");
+        console.log("Cloudinary response:", response);
+        return response;
+    }
+    catch (error) {
+        fs.unlink(localFilePath); // delete the local file after upload attempt as the upload failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
